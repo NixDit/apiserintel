4e87ea1b8130d0aea889c456c7371d3819cf0bc2
Josmar Salvador <josmarmp96@gmail.com>
2021-09-17 17:01:04 -0500

    Alter to Route Seeder

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -15,8 +15,9 @@ class RouteSeeder extends Seeder
      */
     public function run()
     {
+        // BEGIN:: LUNES
         $route = Route::create([
-            'name'          => 'Cañada',
+            'name'          => 'Tehuacan',
             'employee_id'   => 2,
             'day'           => 1
         ]);
@@ -30,5 +31,158 @@ class RouteSeeder extends Seeder
             'route_id'  => $route->id,
             'user_id'   => 4
         ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 6
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 7
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 8
+        ]);
+        // END:: LUNES
+
+
+        // BEGIN:: MARTES
+        $route = Route::create([
+            'name'          => 'Tehuacan Centro',
+            'employee_id'   => 2,
+            'day'           => 2
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 8
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 7
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 6
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+        // END:: MARTES
+
+        // BEGIN:: MIERCOLES
+        $route = Route::create([
+            'name'          => 'Tehuacan Centro Miercoles',
+            'employee_id'   => 2,
+            'day'           => 3
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 6
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 7
+        ]);
+        // END:: MIERCOLES
+
+        // BEGIN:: JUEVES
+        $route = Route::create([
+            'name'          => 'Tehuacan Jueves',
+            'employee_id'   => 2,
+            'day'           => 4
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 3
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 7
+        ]);
+        // END:: JUEVES
+
+        // BEGIN:: VIERNES
+        $route = Route::create([
+            'name'          => 'Tehuacan Ruta Centro Viernes',
+            'employee_id'   => 2,
+            'day'           => 5
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 3
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 4
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+        // END:: VIERNES
+
+        // BEGIN:: SABADO
+        $route = Route::create([
+            'name'          => 'Tehuacan Sabado',
+            'employee_id'   => 2,
+            'day'           => 6
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 3
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 4
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 5
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 6
+        ]);
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 7
+        ]);
+
+        RouteClient::create([
+            'route_id'  => $route->id,
+            'user_id'   => 8
+        ]);
+        // END:: LUNES
+
+
+
+
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -40,10 +40,10 @@ class UserSeeder extends Seeder
 
         //BEGIN:: Create a client
         $client = User::create([
-            'name'      => 'Josmar Salvador',
-            'last_name' => 'Marroquin Parra',
-            'email'     => 'josmarmp96@gmail.com',
-            'password'  => Hash::make('Cliente@2021'),
+            'name'      => 'Abarrotes ',
+            'last_name' => 'La corona',
+            'email'     => 'lacorona@gmail.com',
+            'password'  => Hash::make('lacorona'),
         ]);
         
         $client->assignRole('client');
@@ -51,20 +51,20 @@ class UserSeeder extends Seeder
         $client_code = 'S-' . (str_pad( $client->id, 10, '0', STR_PAD_LEFT));
 
         $client->clientInformation()->create([
-            'business_name' => 'Josmii & Glass Videogames',
+            'business_name' => 'Abarrotes la Corona',
             'code'          => $client_code,
-            'phone'         => '2381473627',
-            'address'       => 'Calle Hidalgo 434 Altepexi, Puebla.',
-            'latlng'        => '18.3655904,-97.3017397'
+            'phone'         => '1231231212',
+            'address'       => 'Calle 3 Ote 322',
+            'latlng'        => '18.4606981,-97.3925737'
         ]);
         //END:: Create a client
 
         //BEGIN:: Create a client
         $second_client = User::create([
-            'name'      => 'Juan Pablo',
-            'last_name' => 'Bonilla Mendez',
-            'email'     => 'jpm_caprico@gmail.com',
-            'password'  => Hash::make('Cliente@2021'),
+            'name'      => 'Abarrotes Rosy',
+            'last_name' => 'Mendez',
+            'email'     => 'rosy@gmail.com',
+            'password'  => Hash::make('rosy'),
         ]);
         
         $second_client->assignRole('client');
@@ -72,32 +72,96 @@ class UserSeeder extends Seeder
         $second_client_code = 'S-' . (str_pad( $second_client->id, 10, '0', STR_PAD_LEFT));
 
         $second_client->clientInformation()->create([
-            'business_name' => 'NIX Desarrollo, Innovacion y Tecnologia',
+            'business_name' => 'Abarrotes Rosy',
             'code'          => $second_client_code,
-            'phone'         => '2361201744',
-            'address'       => 'Andador C 80 Ajalpan, Puebla',
-            'latlng'        => '18.376444,-97.275152'
+            'phone'         => '0980980909',
+            'address'       => 'Calle 3 Poniente',
+            'latlng'        => '18.4611909,-97.4024359'
         ]);
         //END:: Create a client
 
          //BEGIN:: Create a client
          $third_client = User::create([
-            'name'      => 'Adan',
-            'last_name' => 'Marroquin Mendez',
-            'email'     => 'adan_marroquin@gmail.com',
-            'password'  => Hash::make('Marroquin@2021'),
+            'name'      => 'Abarrotes el Gallo',
+            'last_name' => 'Tehuacan',
+            'email'     => 'elgallo@gmail.com',
+            'password'  => Hash::make('elgallo'),
         ]);
         
         $third_client->assignRole('client');
 
-        $second_client_code = 'S-' . (str_pad( $third_client->id, 10, '0', STR_PAD_LEFT));
+        $third_client_code = 'S-' . (str_pad( $third_client->id, 10, '0', STR_PAD_LEFT));
 
         $third_client->clientInformation()->create([
-            'business_name' => 'GYM Kajoma',
-            'code'          => $second_client_code,
-            'phone'         => '2361201743',
-            'address'       => 'Calle Insurgentes 619 Altepexi, Puebla',
-            'latlng'        => '18.3683379,-97.302974'
+            'business_name' => 'Abarrotes el gallo de tehuacan',
+            'code'          => $third_client_code,
+            'phone'         => '2342344556',
+            'address'       => 'Calle 3 Ote 535',
+            'latlng'        => '18.4609287,-97.3899697'
+        ]);
+        //END:: Create a client
+
+        //BEGIN:: Create a client
+        $fourth_client = User::create([
+            'name'      => 'Abarrotes conchita',
+            'last_name' => 'Lopez',
+            'email'     => 'conchita@gmail.com',
+            'password'  => Hash::make('conchita'),
+        ]);
+        
+        $fourth_client->assignRole('client');
+
+        $fourth_client_code = 'S-' . (str_pad( $fourth_client->id, 10, '0', STR_PAD_LEFT));
+
+        $fourth_client->clientInformation()->create([
+            'business_name' => 'Abarrotes Conchita',
+            'code'          => $fourth_client_code,
+            'phone'         => '5675675675',
+            'address'       => 'Calle 7 Sur ',
+            'latlng'        => '18.4586785,-97.389476'
+        ]);
+        //END:: Create a client
+
+        //BEGIN:: Create a client
+        $client = User::create([
+            'name'      => 'Abarrotes Vinos',
+            'last_name' => 'Licores',
+            'email'     => 'vinos@gmail.com',
+            'password'  => Hash::make('vinos'),
+        ]);
+        
+        $client->assignRole('client');
+
+        $client_code = 'S-' . (str_pad( $client->id, 10, '0', STR_PAD_LEFT));
+
+        $client->clientInformation()->create([
+            'business_name' => 'Abarrotes Vinos y Licores Fandango',
+            'code'          => $client_code,
+            'phone'         => '2345862038',
+            'address'       => 'Calle 5 Ote 524',
+            'latlng'        => '18.4585225,-97.3904625'
+        ]);
+        //END:: Create a client
+
+
+        //BEGIN:: Create a client
+        $client = User::create([
+            'name'      => 'Mueganos',
+            'last_name' => 'Aguila',
+            'email'     => 'aguila@gmail.com',
+            'password'  => Hash::make('aguila'),
+        ]);
+        
+        $client->assignRole('client');
+
+        $client_code = 'S-' . (str_pad( $client->id, 10, '0', STR_PAD_LEFT));
+
+        $client->clientInformation()->create([
+            'business_name' => 'Mueganos El Aguila',
+            'code'          => $client_code,
+            'phone'         => '4759683726',
+            'address'       => 'Calle 3 Sur 519',
+            'latlng'        => '18.4579508,-97.393339'
         ]);
         //END:: Create a client
 
