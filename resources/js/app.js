f69ad2e630055c4e0c824910b326750beb3954df
JPablo <juan@teknik.mx>
2022-07-14 21:46:00 -0500

    Fix feature: Set vue 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
-// require('./bootstrap');
-import { createApp } from 'vue';
-// INIT VUE
-let app = createApp({});
+window.Vue = require('vue').default; // Vue
+
 // COMPONENTS
-app.component('login-form',require('./components/auth/login/index.vue').default);
-app.component('register-form',require('./components/auth/register/index.vue').default);
-// MOUNT
-app.mount('#app');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default); // Example
+Vue.component('login-form', require('./components/auth/login/index.vue').default); // Login form
+Vue.component('register-form', require('./components/auth/register/index.vue').default); // Register form
+
+// INITIALIZING VUE
+const app = new Vue({
+    //
+}).$mount('#app');
