4e73aadc5a3964b5f551c11a560c864f7782407d
Juan Pablo Bonilla Mendez <juan@teknik.mx>
2022-08-21 22:37:04 -0500

    Fix: Home route url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/dashboard-app-serintel';
 
     /**
      * The controller namespace for the application.
